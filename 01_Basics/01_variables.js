05e13d6b895f1fb4e1c67478d0863f2759a01088
Vraj Clerk <129680505+vrajclerk@users.noreply.github.com>
2023-12-10 14:22:02 +0530

    Update 01_variables.js

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,15 +1,15 @@
 const accountId = 144553
-let accountEmail = "hitesh@google.com"
+let accountEmail = "vraj@gmail.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "Surat"
 let accountState;
 
 // accountId = 2 // not allowed
 
 
-accountEmail = "hc@hc.com"
+accountEmail = "it021@ddu.com"
 accountPassword = "21212121"
-accountCity = "Bengaluru"
+accountCity = "Nadiad"
 
 console.log(accountId);
 
